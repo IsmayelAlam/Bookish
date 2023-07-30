c0e61ff97e8357068258481bb8e751315a49c0b3
IsmayelAlam <ismayelalam0@gmail.com>
2023-07-30 21:05:12 +0600

    add book details and subjects

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,10 @@
 import { useParams } from "react-router-dom";
 import { BiGlobe } from "react-icons/bi";
+import {
+  AiOutlineCreditCard,
+  AiOutlineFileDone,
+  AiOutlineStar,
+} from "react-icons/ai";
 import { BsBookmarks } from "react-icons/bs";
 import { IoLogoAmazon } from "react-icons/io5";
 import { TbBan } from "react-icons/tb";
@@ -11,6 +16,7 @@ import {
   useGetBookWork,
 } from "../hooks/useGetBook";
 import ButtonLink from "../ui/Button";
+import MiniDetailCards from "../ui/MiniDetailCards";
 
 export default function Book() {
   const { id } = useParams();
@@ -25,13 +31,15 @@ export default function Book() {
 
   const allData = { ...bookData.data, ...bookWorkData.data };
 
+  console.log(allData.authors);
+
   const description = allData?.description?.value || allData?.description;
 
   const cover = `https://covers.openlibrary.org/b/id/${allData?.covers?.[0]}-L.jpg`;
 
   return (
     <div className="px-40 py-14 grid grid-cols-[1fr,3fr,1fr] min-h-screen gap-5">
-      <div className="w-full h-full flex items-center flex-col gap-5 rounded-2xl">
+      <div className="w-full min-w-max h-full flex items-center flex-col gap-5">
         <img src={cover} alt="" className="h-96 mb-10 rounded-lg" />
         <ButtonLink
           link={iaIdentity && `https://archive.org/details/${iaIdentity}`}
@@ -50,11 +58,56 @@ export default function Book() {
         />
       </div>
 
-      <div>
-        <h2>{allData.title}</h2>
-        <p>{allData.number_of_pages}</p>
-        <p>{allData.publish_date}</p>
+      <div className="flex flex-col items-start gap-16">
+        <div>
+          <h2 className="text-5xl">{allData.title}</h2>
+          <div className="flex items-center gap-1 text-lg">
+            <AiOutlineStar />
+            <p>{bookRating?.data?.summary.average?.toFixed(2)}</p>
+            <span className="text-sm">({bookRating?.data?.summary.count})</span>
+          </div>
+          <p>{allData.number_of_pages || 0}</p>
+          <p>{allData.publish_date || 0}</p>
+          <p>publishers: {allData.publishers || "-"}</p>
+        </div>
+
+        <div className="flex capitalize gap-4 my-5">
+          <MiniDetailCards
+            text={`${bookshelves?.data?.counts.already_read || 0} already read`}
+            icon={<AiOutlineFileDone />}
+          />
+          <MiniDetailCards
+            text={`${
+              bookshelves?.data?.counts.currently_reading || 0
+            } currently reading`}
+            icon={<AiOutlineFileDone />}
+          />
+          <MiniDetailCards
+            text={`${bookshelves?.data?.counts.want_to_read || 0} want to read`}
+            icon={<AiOutlineFileDone />}
+          />
+          <MiniDetailCards
+            text={`${bookWorkEdition?.data?.entries.length || 0} editions`}
+            icon={<AiOutlineCreditCard />}
+          />
+        </div>
+
+        <div className="flex flex-wrap my-5 gap-2">
+          <span>Subjects: </span>
+          {allData.subjects
+            ?.filter((subject) => !subject.includes(" "))
+            .map((subject) => (
+              <p key={subject} className="bg-white rounded-full py-1 px-2">
+                {subject}
+              </p>
+            ))}
+        </div>
+
         <p>{description}</p>
+      </div>
+
+      <div className="w-full h-full">
+        <h3>author</h3>
       </div>
     </div>
   );
--- src/ui/MiniDetailCards.jsx
+++ src/ui/MiniDetailCards.jsx
@@ -1,7 +1,7 @@
-export default function MiniDetailCards({ icon, text, color }) {
+export default function MiniDetailCards({ icon, text }) {
   return (
     <div
-      className={`flex items-center py-2 px-1 rounded-lg bg-yellow-300 gap-1 truncate`}
+      className={`flex items-center py-2 px-2 rounded-lg bg-yellow-300 gap-1 truncate`}
     >
       <div className={`border-2 border-yellow-800 p-2 rounded-full`}>
         {icon}
