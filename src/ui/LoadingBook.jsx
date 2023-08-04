65b69cfc3336365e030a17c95d7b8cd000b12bff
IsmayelAlam <ismayelalam0@gmail.com>
2023-08-04 19:44:55 +0600

    ready for deploy

--- src/ui/LoadingBook.jsx
+++ src/ui/LoadingBook.jsx
@@ -1,9 +1,9 @@
-import { GiBookmarklet } from "react-icons/gi";
+import { GiBlackBook } from "react-icons/gi";
 
 export default function LoadingBook() {
   return (
     <div className="text-grayish01/50 w-screen flex justify-center items-center h-[80vh]">
-      <GiBookmarklet className="w-80 h-80 animate-pulse" />
+      <GiBlackBook className="w-80 h-80 animate-pulse" />
     </div>
   );
 }
